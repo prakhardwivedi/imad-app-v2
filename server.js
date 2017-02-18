4d67c9e280c78e517d346b453cddfe3382ed5fd8
prakhardwivedi <Pd19april@gmail.com>
2017-02-18 13:37:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,20 +2,31 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var app = express();
+app.use(morgan("combined"));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article one.html'));
+   rnd('Article one requested and will be served here');
 });
 
 app.get('/article-two',function(req,res) {
-    res.send('Article two requested and will be served here');
+   rnd('Article two requested and will be served here');
 });
 
 app.get('/article-three',function(req,res) {
     res.send('Article three requested and will be served here');
+});
+
+app.get('/ui/style.css',function (req,res) {
+    res.sendfile(path.join(__dirname,'ui','style.css'));
+});
+
+app.get('/ui/madi.png',function(req,res){
+    res.sendfile(path.join(__dirname,'ui','madi.png'));
 });
 
 
