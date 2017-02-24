5908506350d207d7f7a547025b1f460880a7012b
prakhardwivedi <Pd19april@gmail.com>
2017-02-24 16:50:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,11 @@ element.innerHTML = 'New Value';
 
 //Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft = 'px';
+}
 img.onclick = function () {
-    img.style.marginleft = '100px';
+    var interval = setinterval(moveright, 100);
 };
